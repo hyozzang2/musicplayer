ff4ea280470ccdfe14b292c5956cc04fe3d47878
hyozzang2 <118510760+hyozzang2@users.noreply.github.com>
2022-11-25 13:54:30 +0900

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,17 +1,31 @@
 let musicFile;
+let button;
 
 
 function setup()
 {
-   soundFormats('mp3', 'ogg');
-   musicFile = loadSound('music');
+    createCanvas(200,200);
+    song=loadSound('music.mp3',loaded);
+    buton=createButton('play');
+    button.mousePressed(togglePlaying);
+    background(51);
+//    soundFormats('mp3', 'ogg');
+//    musicFile = loadSound('music', LoadMusic);
 //    musicFile.play();
-
 
 }
 
-function loadMusic()
-{
-    musicFile.play();
+function togglePlaying(){
+    if(!song.isPlaying()){
+        song.play()
+        song.setVolume(0.3);
+        button.html('pause');
+    }else{
+        song.stop();
+        button.html('play');
+    }
+}
 
+function loaded(){
+    console.log('loaded');
 }
