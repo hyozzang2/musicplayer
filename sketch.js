eca18e6dd958aaa2cd807e488e4c596aa9bd3e85
hyozzang2 <118510760+hyozzang2@users.noreply.github.com>
2022-12-02 12:45:46 +0900

    완벽
    굿

--- sketch.js
+++ sketch.js
@@ -1,22 +1,16 @@
 let musicFile;
 let button;
-
-
 function setup()
 {
-    createCanvas(640,480);
+    createCanvas(640, 480);
     soundFormats('mp3', 'ogg');
-    musicFile = loadSound('music.mp3', loadMusic);
-    
-    button=createButton('play');
+    musicFile = loadSound('mp3.mp3',loadMusic);
+    button = createButton('play');
     button.mousePressed(togglePlaying);
- 
-
 }
-
 function togglePlaying(){
     if(!musicFile.isPlaying()){
-        musicFile.play()
+        musicFile.play();
         button.html('pause');
     }else{
         musicFile.stop();
@@ -24,9 +18,11 @@ function togglePlaying(){
     }
 }
 
-function loadMusic(){
-    console.log("It's working");
+function loadMusic()
+{
+    console.log("It's Working");
 }
+
 function draw(){
     fill(255,0,0);
     ellipse(50,50,100,100);
